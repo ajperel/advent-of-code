4d07c4ac8f6247f8cb0dbddc557069732778bf5e
Andy Perelson <ajperel@gmail.com>
2022-01-08 17:18:21 -0800

    Add support for multiple input files.

--- 2021/lib/get-input.js
+++ 2021/lib/get-input.js
@@ -5,9 +5,9 @@ const path = require('path');
  * Returns the contents of 'input.txt' in the current directly as an array of strings one per line
  * of the file.
  */
-function getInput({delimiter = '\n'} = {}) {
+function getInput({delimiter = '\n', filename = 'input.txt'} = {}) {
   const currentDirectory = path.dirname(module.parent.filename);
-  let inputFileName = path.resolve(currentDirectory, 'input.txt');
+  let inputFileName = path.resolve(currentDirectory, filename);
 
   let fileArray = fs.readFileSync(inputFileName).toString().trim().split(delimiter);
 
