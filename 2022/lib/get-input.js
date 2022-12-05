eb95cee0047942f480afa581174c908e44735697
Andy Perelson <ajperel@gmail.com>
2022-12-04 22:30:44 -0800

    Only trim end of input.

--- 2022/lib/get-input.js
+++ 2022/lib/get-input.js
@@ -5,11 +5,11 @@ const path = require('path');
  * Returns the contents of 'input.txt' in the current directly as an array of strings one per line
  * of the file.
  */
-function getInput({delimiter = '\n', filename = 'input.txt'} = {}) {
+function getInput({delimiter = '\n', filename = 'input.txt', trim = true} = {}) {
   const currentDirectory = path.dirname(module.parent.filename);
   let inputFileName = path.resolve(currentDirectory, filename);
 
-  let fileArray = fs.readFileSync(inputFileName).toString().trim().split(delimiter);
+  let fileArray = fs.readFileSync(inputFileName).toString().trimEnd().split(delimiter);
 
   return fileArray;
 }
